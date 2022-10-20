eee7c6b4ffb731cef3f27b239d1a4ed8d7cb1349
gsubram <govindaraj_subramani@gap.com>
2022-10-20 14:36:59 +0530

    GCP-Hackathon

---
+++ src/main/java/com/nisum/hackathon/weatherApp/controller/WeatherController.java
@@ -0,0 +1,28 @@
+package com.nisum.hackathon.weatherApp.controller;
+
+import com.nisum.hackathon.weatherApp.services.WeatherService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("weather")
+public class WeatherController {
+
+    @Autowired
+    private WeatherService weatherService;
+
+    @ApiOperation("Return a JSON object that gives the weather averages.")
+    @CrossOrigin(origins = "*")
+    @GetMapping(value = "/forecast", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> weatherForecastAverage(@ApiParam("City's name") @RequestParam(required = true) String city) {
+        return weatherService.weatherForecastAverage(city);
+    }
+}
